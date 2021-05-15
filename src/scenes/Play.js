de73ba9a1e80f19d37f42f0f515addf2eb07e98c
Kendrick <kwlong@ucsc.edu>
2021-05-14 21:18:02 -0700

    update tiles, depth, & map

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,12 +5,13 @@ class Play extends Phaser.Scene {
 
     preload(){
         this.load.image("tiles_01", "./assets/img/town_tilemap_01.png");
-        this.load.tilemapTiledJSON("map_01", "./assets/config/map_01.json" );
+        this.load.tilemapTiledJSON("map_01", "./assets/config/map01.json" );
     }
 
     create() {
         cursors = this.input.keyboard.createCursorKeys();
         this.textbox = new TextBox(this, "This is a super long text to check the width depth of the textbox", 'text_box');
+        //this.textbox.setDepth(10);
 
         this.player = new Player(this);
         this.player.x = 16;
@@ -20,9 +21,12 @@ class Play extends Phaser.Scene {
         const map = this.make.tilemap({key: "map_01"});
         const tileset01 = map.addTilesetImage("town_tileset_01", "tiles_01");
         //establishing layers
-        const bgLayer = map.createStaticLayer("bg", tileset01, 0, 0);
-        const groundLayer = map.createStaticLayer("ground", tileset01, 0, 0);
-        const frontLayer = map.createStaticLayer("front", tileset01, 0, 0);
+        const bgLayer = map.createLayer("bg", tileset01, 0, 0);
+        bgLayer.setDepth(-1);
+        const groundLayer = map.createLayer("ground", tileset01, 0, 0);
+        groundLayer.setDepth(-1);
+        const frontLayer = map.createLayer("front", tileset01, 0, 0);
+        frontLayer.setDepth(-1);
 
     }
 
