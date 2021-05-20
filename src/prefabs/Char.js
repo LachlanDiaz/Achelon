45685e0ef8f9e97a9672f8bad76854d8f7145d0e
laadiaz <laadiaz@ucsc.edu>
2021-05-19 22:28:14 -0700

    fixed textbox and char postion

--- src/prefabs/Char.js
+++ src/prefabs/Char.js
@@ -1,6 +1,6 @@
 class Char extends Phaser.Physics.Arcade.Sprite {
     constructor(scene) {
-        super(scene);
+        super(scene, 96, 96);
 
         scene.add.existing(this);
         scene.physics.add.existing(this);
