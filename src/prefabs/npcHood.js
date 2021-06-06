3d11cc62699f226fc58b9609f3c0ab54265a3d63
Kendrick <kwlong@ucsc.edu>
2021-06-05 21:07:21 -0700

    big changes :o

--- src/prefabs/npcHood.js
+++ src/prefabs/npcHood.js
@@ -10,8 +10,6 @@ class npcHood extends Phaser.Physics.Arcade.Sprite {
         this.scene.temp;
         this.end_convo = false;
         this.anims.play("hood_idle");
-        this.soulFound == false;
-        this.bloodFound == false;
     }
 
 
@@ -23,10 +21,10 @@ class npcHood extends Phaser.Physics.Arcade.Sprite {
                 this.scene.temp = new TextBox(this.scene, ["...", ".....", ".......",
                     "Their bleeding eyes...", "...watch the mist.", ""], 'text_box');
                 this.end_convo = true;
-            }else if (this.end_convo && this.soulFound == false && this.bloodFound == true){
+            } else if (this.end_convo == true && inventory.has("Stone Eyes") && !inventory.has("Fading Soul")){
                 this.scene.temp = new TextBox(this.scene, ["...", "The blind one wants...", "...what the watcher had...", ""], 'text_box');
-            } else if (this.end_convo && this.soulFound == true) {
-                this.scene.temp = new TextBox(this.scene, [".....", "Nothing above...", "Nothing at all.", ""], 'text_box');
+            } else if (this.end_convo == true && inventory.has("Fading Soul")) {
+                this.scene.temp = new TextBox(this.scene, [".....", "Nothing above...", "Nothing at all...", ""], 'text_box');
             } else{
                 this.scene.temp = new TextBox(this.scene, ["...", "Bleeding eyes...", "...watching the mist.", ""], 'text_box');
             }
